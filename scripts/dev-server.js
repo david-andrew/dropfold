85750ecaac7b37279ec356f7806a84db9711887d
David Samson <david.andrew.engineer@gmail.com>
2024-08-17 18:03:26 -0400

    added paper folding scene

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -2,7 +2,7 @@ var liveServer = require("live-server");
 
 var params = {
 	root: "./dist",	// builds get output to dist/
-	wait: 500,   	// wait for things to settle before reloading
+	wait: 1000,   	// wait for things to settle before reloading
 	// open: false, 	// don't open the browser
 
 };
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import * as THREE from 'three';
 
+type SceneFunctions = { 
+    update_scene: (renderer:THREE.WebGLRenderer) => void, 
+    camera: THREE.PerspectiveCamera,
+}
 
 const main = () => {
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const { update_scene, on_resize } = rotating_cube_scene();
+    const { update_scene, camera } = paper_folding_scene(); //rotating_cube_scene();
+    const on_resize = () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
 
     const animate = () => {
         requestAnimationFrame(animate);
         update_scene(renderer);
     }
-
     animate();
 
     // Adjust the scene when the window is resized
@@ -22,9 +29,32 @@ const main = () => {
     });
 }
 
+const paper_folding_scene = (): SceneFunctions => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const geometry = new THREE.BoxGeometry(8.5, 11, 0.01);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const paper = new THREE.Mesh(geometry, material);
+
+    scene.add(paper);
+
+    camera.position.z = 10;
+
+    const update_scene = (renderer:THREE.WebGLRenderer) => {
+        //TODO: replace with click to orbit
+        // paper.rotation.x += 0.01;
+        // paper.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    }
+
+    return { update_scene, camera }
+}
 
 
-const rotating_cube_scene = () => {
+
+const rotating_cube_scene = (): SceneFunctions => {
     // Create a scene
     const scene = new THREE.Scene();
 
@@ -51,12 +81,7 @@ const rotating_cube_scene = () => {
         renderer.render(scene, camera);
     }
 
-    const on_resize = () => {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-    }
-
-    return { update_scene, on_resize }
+    return { update_scene, camera }
 }
 
 
