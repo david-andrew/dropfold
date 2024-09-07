95fd438b78e81c0aff8826a9569dd43da11ee22e
David Samson <david.andrew.engineer@gmail.com>
2024-09-06 22:09:36 -0400

    made business card scene use build thing instead of old paper folding scene

--- src/scenes/business_card.ts
+++ src/scenes/business_card.ts
@@ -1,16 +1,16 @@
 import * as THREE from 'three';
-import { general_folding_scene } from './paper_folding_3';
+// import { general_folding_scene } from './paper_folding_3';
+import {build_thing_from_seed} from './build_thing'
+import { OrbitalPointer } from '../controls';
 
-
-
-export const business_card = general_folding_scene(
+export const business_card = build_thing_from_seed(
     [
         [-1.75, 1],
         [1.75, 1],
         [1.75, -1],
         [-1.75, -1], 
     ],
-    0x000000,
-    0x000000,
-    0xffffff,
+    // 0x000000,
+    // 0x000000,
+    // 0xffffff,
 )
